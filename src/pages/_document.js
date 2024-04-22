fd4d9b7de6576747354d3322d0be15510af4eb59
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-04-22 15:34:58 +0530

    Working on

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+<script src="https://mediafiles.botpress.cloud/3d75750b-5496-46c1-9849-0eb2d0fd09db/webchat/config.js" defer></script>
+      </body>
+    </Html>
+  );
+}
